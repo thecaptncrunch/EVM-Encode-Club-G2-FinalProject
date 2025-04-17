477b1e31a8dfc6951794a012c219ee2174b610d5
Loto-lang <laurenttho@gmail.com>
2025-04-17 20:10:48 +0200

    v0.2: added mint script + metadata NFT soulbound

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ npx ts-node --files ./scripts/CertificateDeployWithViem.ts
 Deploying contract with address: 0x7D5d9B00eF4D244dE938a156cA206CC5175C2bF8
 Transaction hash: 0x200f2f654453d62cf23dca8aebf77b13751e862c58d629df21461e5d8283a22b
 ✅ Contract deployed at: 0xafa687762f9b178d73f9564a08cad2a83d69ab60
+
+First NFT minted
+Minting certificate to 0x3B79764C76c5ae3c4B39a4049c53f512ee30eF3c 
+with URI: ipfs://bafkreiaavrym4gselelazif4r4wybi2vybwwg6nznwzr6xa6u6y6jdgste
+✅ Transaction hash: 0x8805dd23987b4e06a146a6ae60baa70738e2b882d9f3e85f9ede237543aab64c
---
+++ scripts/mintWithViem.ts
@@ -0,0 +1,46 @@
+import { createWalletClient, http } from 'viem';
+import { privateKeyToAccount } from 'viem/accounts';
+import { sepolia } from 'viem/chains';
+import { writeContract } from 'viem/actions';
+import dotenv from 'dotenv';
+import contractJson from '../artifacts/contracts/CertificateNFT.sol/CertificateNFT.json';
+
+dotenv.config();
+
+async function main() {
+  const contractAddress = process.env.NFT_Key as `0x${string}`;
+  if (!contractAddress) {
+    throw new Error("❌ Missing NFT_Key in .env");    
+  }
+  const contractAddressTyped = contractAddress as `0x${string}`;
+
+
+  const recipient = "0x3B79764C76c5ae3c4B39a4049c53f512ee30eF3c" as `0x${string}`;
+  const certURI = "ipfs://bafkreiaavrym4gselelazif4r4wybi2vybwwg6nznwzr6xa6u6y6jdgste";
+
+  const privateKey = `0x${process.env.PRIVATE_KEY!}` as `0x${string}`;
+  const account = privateKeyToAccount(privateKey);
+
+  const walletClient = createWalletClient({
+    account,
+    chain: sepolia,
+    transport: http(process.env.RPC_ENDPOINT_URL!)
+  });
+
+  console.log(`🧾 Minting certificate to ${recipient} with URI: ${certURI}`);
+
+  const hash = await writeContract(walletClient, {
+    abi: contractJson.abi,
+    address: contractAddressTyped as `0x${string}`,
+    functionName: 'issueCertificate',
+    args: [recipient, certURI],
+    account,
+  });
+
+  console.log('✅ Transaction hash:', hash);
+}
+
+main().catch((err) => {
+  console.error('❌ Mint failed:', err);
+  process.exit(1);
+});
